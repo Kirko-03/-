e3da86915f163fa6c7eeb79bf8e888d3198bd0a2
Kirito <kurulluvarov@gmail.com>
2022-10-01 08:56:58 +0300

    Settings success

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-counter',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class CounterComponent {
   title = 'counter';
+  max =  localStorage.getItem('max')||10
+
   counter = 0
+  firstValue = localStorage.getItem('firstValue')||0
+  min = localStorage.getItem('max')||-10
   incr(){
     this.counter--
   }
   returning(){
-    this.counter = 0
+    this.counter = Number(this.firstValue)
+    this.max = 10
+    this.min = -10
   }
   decr(){
     this.counter++
   }
+  saving(){
+    
+    Number(localStorage.setItem('max', this.max+''));
+    Number(localStorage.setItem('min', this.min+''));
+    Number(localStorage.setItem('firstValue', this.firstValue+''));
+  }
+  maximum(){
+    this.max = this.counter
+    this.max = Number(localStorage.setItem('max', this.max+''));
+  }
+  minimum(){
+    this.min = this.counter
+    this.min = Number(localStorage.setItem('min', this.min+''));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {CounterComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CounterComponent,
+
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CounterComponent]
 })
 export class AppModule { }
